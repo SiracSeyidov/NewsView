72de0e9c364b469ce96547a7cb352181a8226449
SeyidovSirac <60558048+SiracSeyidov@users.noreply.github.com>
2023-10-26 17:43:59 +0300

    Initial Commit

---
+++ NewsView/NewsViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewApp.swift
+//  NewsView
+//
+//  Created by Sirac Seyidov on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
